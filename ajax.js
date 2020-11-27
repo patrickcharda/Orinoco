56e4372559e0c25604358162cdba37748e75af78
Patrick <patrick.chardavoine@gmail.com>
2020-11-27 19:37:25 +0100

    tests dialogue api /order

--- ajax.js
+++ ajax.js
@@ -14,4 +14,25 @@ class Ajax {
             request.send();
         });
     }
+
+    static post(url, params) {
+        console.log(url);
+        console.log(params[0]);
+        console.log(params[1]);
+        return new Promise(function(resolve, reject) {
+            var request = new XMLHttpRequest();
+            request.onreadystatechange = function () {
+                if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+                    var response = JSON.parse(this.responseText);
+                    //alert('echo');
+                    //console.log(response);
+                    resolve(response);
+                }
+            };
+            console.log(params[0] + ' param0');
+            request.open("POST", url);
+            request.setRequestHeader("Content-Type", "application/json");
+            request.send(params[0],params[1]);
+        });
+    }
 }
--- display.js
+++ display.js
@@ -1,47 +1,74 @@
 class Display {
     constructor() {
-      this.divMeubles = document.getElementById("divMeubles");
-      this.divResult = document.getElementById("result");
+        this.divMeubles = document.getElementById("divMeubles");
+        this.divDetailMeuble = document.getElementById("divDetailMeuble");
+        this.divOrder = document.getElementById("divOrder");
     }
   
     async displayMeubles() {
-      let listeMeubles = await Ajax.get("http://localhost:3000/api/furniture/");
-      console.log(listeMeubles);
-      const items = document.createElement("div");
-      items.setAttribute("id", "divMeubles");
+        let listeMeubles = await Ajax.get("http://localhost:3000/api/furniture/");
+        console.log(listeMeubles);
+        //const items = document.createElement("div");
+        //items.setAttribute("id", "divListeMeubles");
 
-      for (let meuble of listeMeubles) {
+        for (let meuble of listeMeubles) {
     
+            const item = document.createElement("div");
+            item.setAttribute("class", "divMeuble");
+            item.innerHTML = meuble._id + ' , ' + meuble.name + ' , ' + meuble.price + '<br>' + meuble.description + '<br><br>';
+        
+            const divImgMeuble = document.createElement("div");
+            divImgMeuble.setAttribute("class","divImgMeuble");
+        
+            const lienImg = document.createElement("a");
+            lienImg.setAttribute("href", "./products/produit.html?id="+meuble._id);
+            lienImg.setAttribute("class","lienImg");
+    
+            const fitImg = document.createElement("img");
+            fitImg.setAttribute("src", meuble.imageUrl);
+            fitImg.setAttribute("class","fitImg");
+        
+            divImgMeuble.appendChild(lienImg);
+            divImgMeuble.appendChild(fitImg);
+        
+            item.appendChild(divImgMeuble);
+            this.divMeubles.appendChild(item);
+        };
+    }
+
+    async displayMeuble(id) {
+
+        let detailMeuble = await Ajax.get("http://localhost:3000/api/furniture/"+id);
+        console.log(detailMeuble);
+        //this.divDetailMeuble.textContent = detailMeuble;
         const item = document.createElement("div");
         item.setAttribute("class", "divMeuble");
-        item.innerHTML = meuble._id + ' , ' + meuble.name + ' , ' + meuble.price + '<br>' + meuble.description + '<br><br>';
-    
+        item.innerHTML = detailMeuble._id + ' , ' + detailMeuble.name + ' , ' + detailMeuble.price + '<br>' + detailMeuble.description + '<br><br>';
+        
         const divImgMeuble = document.createElement("div");
         divImgMeuble.setAttribute("class","divImgMeuble");
-    
+        
         const lienImg = document.createElement("a");
-        //lienImg.setAttribute("href", "./products/produit.html?id="+meuble._id);
-        lienImg.setAttribute("href", "./products/produit.html");
+        lienImg.setAttribute("href", "./products/produit.html?id="+detailMeuble._id);
         lienImg.setAttribute("class","lienImg");
-        lienImg.setAttribute("id", meuble._id);
-        //créer un objet contenant la référence du meuble et l'exporter ?
-        lienImg.addEventListener('click', function(event) {
-          //askMeuble(meuble._id);
-          event.preventDefault();
-          askMeuble(meuble._id);
-          //alert('toto');
-        });
     
         const fitImg = document.createElement("img");
-        fitImg.setAttribute("src", meuble.imageUrl);
+        fitImg.setAttribute("src", detailMeuble.imageUrl);
         fitImg.setAttribute("class","fitImg");
-    
+        
         divImgMeuble.appendChild(lienImg);
         divImgMeuble.appendChild(fitImg);
-    
+        
         item.appendChild(divImgMeuble);
-        this.divMeubles.appendChild(item);
-      };
+        this.divDetailMeuble.appendChild(item);
+        
     }
-  
+
+    async displayOrder(contact, products) {
+        const params=[contact, products];
+        console.log(params);
+        var detailOrder = await Ajax.post("http://localhost:3000/api/furniture/order", params);
+        console.log(detailOrder);
+    }
+
   }
