d74dacd08a8d5f6fa58a0343431fdc48938ea4e5
Patrick <patrick.chardavoine@gmail.com>
2020-12-02 10:17:24 +0100

    debut construction panier

--- ajax.js
+++ ajax.js
@@ -15,24 +15,24 @@ class Ajax {
         });
     }
 
-    static post(url, params) {
+    static post(url, contact, products) {
         console.log(url);
-        console.log(params[0]);
-        console.log(params[1]);
+        //console.log(params[0]);
+        //console.log(params[1]);
         return new Promise(function(resolve, reject) {
             var request = new XMLHttpRequest();
             request.onreadystatechange = function () {
                 if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
                     var response = JSON.parse(this.responseText);
                     //alert('echo');
-                    //console.log(response);
+                    console.log(response);
                     resolve(response);
                 }
             };
-            console.log(params[0] + ' param0');
+            //console.log(params[0] + ' param0');
             request.open("POST", url);
             request.setRequestHeader("Content-Type", "application/json");
-            request.send(params[0],params[1]);
+            request.send(contact, products);
         });
     }
 }
--- display.js
+++ display.js
@@ -3,7 +3,16 @@ class Display {
         this.divMeubles = document.getElementById("divMeubles");
         this.divDetailMeuble = document.getElementById("divDetailMeuble");
         this.divOrder = document.getElementById("divOrder");
+        this.divPanier = document.getElementById("divPanier");
     }
+    
+    // Static properties shared by all instances
+    static arrayPanierProducts =[];
+    static objetPanierProducts = {};
+
+    /*static staticMethod() {
+    console.log(this.staticPanierProducts);
+    }*/
   
     async displayMeubles() {
         let listeMeubles = await Ajax.get("http://localhost:3000/api/furniture/");
@@ -23,6 +32,7 @@ class Display {
             const lienImg = document.createElement("a");
             lienImg.setAttribute("href", "./products/produit.html?id="+meuble._id);
             lienImg.setAttribute("class","lienImg");
+            lienImg.setAttribute("draggable","true");
     
             const fitImg = document.createElement("img");
             fitImg.setAttribute("src", meuble.imageUrl);
@@ -34,6 +44,34 @@ class Display {
             item.appendChild(divImgMeuble);
             this.divMeubles.appendChild(item);
         };
+    }
+
+    static ajouterProduit(id) {
+        
+        //sessionStorage.setItem("product",id);
+        //alert("test");
+        console.log(id);
+        Display.arrayPanierProducts.push(id);
+        if (Display.objetPanierProducts[`${id}`] == null) {
+            console.log('res : ' + `${id}`);
+            Display.objetPanierProducts[`${id}`]=1;
+        
+        } else {
+            Display.objetPanierProducts[`${id}`]+=1;
+        }
+        console.log('panier :' + Display.objetPanierProducts[id]);
+        console.log('panier :' + Display.arrayPanierProducts);
+        //afficher nombre d'éléments de l'objet 
+        var longueur =0;
+        for(let key in Display.objetPanierProducts) {
+            longueur+=1;
+        }
+        console.log(longueur);
+        //afficher l'object sous forme de tableau
+        const tab = Object.entries(Display.objetPanierProducts);
+        console.log(tab);
+
+        
     }
 
     async displayMeuble(id) {
@@ -49,7 +87,7 @@ class Display {
         divImgMeuble.setAttribute("class","divImgMeuble");
         
         const lienImg = document.createElement("a");
-        lienImg.setAttribute("href", "./products/produit.html?id="+detailMeuble._id);
+        lienImg.setAttribute("href", "./produit.html?id="+detailMeuble._id);
         lienImg.setAttribute("class","lienImg");
     
         const fitImg = document.createElement("img");
@@ -61,14 +99,56 @@ class Display {
         
         item.appendChild(divImgMeuble);
         this.divDetailMeuble.appendChild(item);
-        
+
+        if (detailMeuble.varnish.length > 0) {
+            const divProductOptions = document.createElement("select");
+            divProductOptions.setAttribute("class","productOptions");
+            divProductOptions.setAttribute("name","productOptions");
+            var option='';
+            for (let i=0;i<detailMeuble.varnish.length;i++) {
+                option += '<option>' + detailMeuble.varnish[i];
+            }
+            divProductOptions.innerHTML = option;
+            this.divDetailMeuble.appendChild(divProductOptions);        
+        }
+
+        const btnAjout = document.createElement("button");
+        btnAjout.innerHTML = 'Ajouter au panier';
+        btnAjout.addEventListener('click', function(event) {
+            event.preventDefault();
+            Display.ajouterProduit(detailMeuble._id);
+        })
+        this.divDetailMeuble.appendChild(btnAjout);
     }
+
+
+    async getMeuble(id) {
+        let detailMeuble = await Ajax.get("http://localhost:3000/api/furniture/"+id);
+        console.log(detailMeuble);
+        var meuble = {
+            "varnish": ["Dark Oak","Mahogany"],
+            "_id": detailMeuble._id,
+            "name": detailMeuble.name,
+            "price": detailMeuble.price,
+            "description": detailMeuble.description,
+            "imageURL": detailMeuble.imageUrl
+        };
+        console.log(meuble);
+        return meuble;
+    }
+
 
     async displayOrder(contact, products) {
         const params=[contact, products];
         console.log(params);
-        var detailOrder = await Ajax.post("http://localhost:3000/api/furniture/order", params);
+        var detailOrder = await Ajax.post("http://localhost:3000/api/furniture/order", contact, products);
         console.log(detailOrder);
     }
+
+    /*async returnAFurnitureObject(id) {
+        let detailMeuble = await Ajax.get("http://localhost:3000/api/furniture/"+id);
+        //console.log(detailMeuble);
+        return detailMeuble.description;
+    }*/
 
   }
